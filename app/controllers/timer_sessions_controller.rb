dd709b3719ada7bf074dfdededba7ae8c3d8235d
Travis Benge <travis.l.benge@gmail.com>
2018-05-29 11:35:33 +0200

    timer_sessions controller

--- app/controllers/timer_sessions_controller.rb
+++ app/controllers/timer_sessions_controller.rb
@@ -1,20 +1,23 @@
 class TimerSessionsController < ApplicationController
   def index
+    @timer_sessions = Timer_session.all
   end
 
   def show
+    @timer_session = Timer_session.find(params[:id])
   end
 
   def new
+    @timer_session = Timer_session.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @timer_session = Timer_session.create(timer_session_params)
+    if timer_session.save
+      redirect_to timer_session_path(@timer_session)
+    else
+      render :new
+    end
   end
 
   def destroy
