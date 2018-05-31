c15aa5af72c72d9efa34ee84b866d31b1544bb47
Travis Benge <travis.l.benge@gmail.com>
2018-05-31 18:47:14 +0200

    finished rough draft of timer

--- app/javascript/packs/easytimer.js
+++ app/javascript/packs/easytimer.js
@@ -1,6 +1,7 @@
 import Timer from 'easytimer'
 
 function redirectToBreakNew(timer) {
+  console.log(targetAchieved(timer))
   if(targetAchieved(timer)){
     const dataSetTimer = document.getElementById("timer").dataset
     const idSession = dataSetTimer.sessionid
@@ -16,10 +17,17 @@ function redirectToBreakNew(timer) {
 }
 
 function targetAchieved(timer) {
-  const target = document.getElementById("timer").dataset.limit
-  console.log(timer.toString())
-  console.log(target)
-  return(timer.toString() === target);
+  const dataSetLimits = document.getElementById("timer").dataset
+  const focusLimit = dataSetLimits.limit
+  const breakLimit = dataSetLimits.breaklimit
+    console.log(breakLimit)
+  if (breakLimit) {
+    return (timer.seconds === parseInt(breakLimit)) ? true : false;
+  } else {
+    console.log(timer.seconds)
+    console.log(parseInt(focusLimit))
+    return (timer.seconds === parseInt(focusLimit)) ? true : false;
+  }
 }
 
 function startTimerSession() {
@@ -33,3 +41,37 @@ function startTimerSession() {
 }
 global.startTimerSession = startTimerSession
 
+// import Timer from 'easytimer'
+
+// function redirectToBreakNew(timer) {
+//   if(targetAchieved(timer)){
+//     const dataSetTimer = document.getElementById("timer").dataset
+//     const idSession = dataSetTimer.sessionid
+//     const idBreak = dataSetTimer.break
+//     if (idBreak) {
+//       window.location.href = `/timer_sessions/${idSession}`
+//     } else {
+//       window.location.href =`/timer_sessions/${idSession}/breaks/new`
+//     }
+//   }
+
+//   // rediredctio to break new
+// }
+
+// function targetAchieved(timer) {
+//   const target = document.getElementById("timer").dataset.limit
+//   console.log(timer.toString())
+//   console.log(target)
+//   return(timer.toString() === target);
+// }
+
+// function startTimerSession() {
+
+//   const timer = new Timer();
+//   timer.start();
+//   timer.addEventListener('secondsUpdated', function (e) {
+//     $('#timer').html(timer.getTimeValues().toString());
+//     redirectToBreakNew(timer.getTimeValues())
+//   });
+// }
+// global.startTimerSession = startTimerSession
