1ec3e6385638097209cf7c783e45ff88e1255538
David Lemaire <lemairedvd@gmail.com>
2018-06-05 18:50:08 +0200

    navbabar

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,5 +1,6 @@
 function changeNavbarOnScroll() {
   const navbar = document.querySelector('.navbar');
+  const screen = document.querySelector('.navbar');
   window.addEventListener("scroll", () => {
     if (window.scrollY >= window.outerHeight) {
       navbar.classList.add('navbar-transparent');
