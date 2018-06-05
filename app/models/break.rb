ad85b1b1217e1d7b684e71553aaeb9360c11067d
Travis Benge <travis.l.benge@gmail.com>
2018-06-05 10:40:21 +0200

    Countdown timer implemented

--- app/models/break.rb
+++ app/models/break.rb
@@ -3,14 +3,14 @@ class Break < ApplicationRecord
   belongs_to :activity, dependent: :destroy
   validates :activity, presence: true
 
-  def duration
-    min = timer_session.preset.break_duration
-    total_seconds = min * 60
-    seconds = total_seconds % 60
-    minutes = (total_seconds / 60) % 60
-    hours = total_seconds / (60 * 60)
+  # def duration
+  #   min = timer_session.preset.break_duration
+  #   total_seconds = min * 60
+  #   seconds = total_seconds % 60
+  #   minutes = (total_seconds / 60) % 60
+  #   hours = total_seconds / (60 * 60)
 
-    format("%02d:%02d:%02d", hours, seconds, minutes) #=> "01:00:00"
-    # "00:00:05"
-  end
+  #   format("%02d:%02d:%02d", hours, seconds, minutes) #=> "01:00:00"
+  #   # "00:00:05"
+  # end
 end
