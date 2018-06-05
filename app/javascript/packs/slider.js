5bdf4ca3b07df13001c682e552b441c35f06643b
Yatan Blu <yatanblu@gmail.com>
2018-06-05 17:57:40 +0200

    updated buttons, all working

---
+++ app/javascript/packs/slider.js
@@ -0,0 +1,4 @@
+import Slider from 'bootstrap-slider';
+new Slider("input#preset_working_day", { min: 0, max: 12 })
+new Slider("input#preset_focus_timer", { min: 0, max: 60 })
+new Slider("input#preset_break_duration", { min: 0, max: 60 })
