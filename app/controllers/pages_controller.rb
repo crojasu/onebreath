eeb16eaf5f43b19c4cca1b9fed6a0c02d7963ccb
Yatan Blu <yatanblu@gmail.com>
2018-06-06 14:35:01 +0200

    dashboard and home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :ui]
 
   def home
     if user_signed_in?
-      redirect_to presets_path
+      redirect_to dash_path
     end
   end
 
   def ui
   end
+
+  def dash
+    @navbar_render = true
+    @user = current_user
+  end
+
+
 end
