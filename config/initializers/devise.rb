a6c2e1fc1ef512afd70f586da7e6cdd18006b576
Yatan Blu <yatanblu@gmail.com>
2018-06-06 17:51:55 +0200

    FB working locally

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,13 +3,19 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # Facebook Login
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+    scope: 'email',
+    info_fields: 'email, first_name, last_name',
+    image_size: 'square',  # 50x50, guaranteed ratio
+    secure_image_url: true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_base` as its `secret_key`
+  # Devise will u
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'd04ad0d28788ba198e5acfb6ad281e6245b6b8c3735e5013f04dbeb0d13b589a2612133507caad9150889bdb82d9223eaeb8e70bbc71a8931aab250097707c68'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
